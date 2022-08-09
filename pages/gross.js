a187389626fcef536e4b97a5e30209b0e3e4a066
goodolchap <chapman7kwan@gmail.com>
2022-08-09 15:17:36 -0700

    first commit

---
+++ pages/gross.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Home.module.css'
+
+export default function Gross () {
+    return (
+    
+        <main className={styles.main}>
+            <ul>
+                <li>Wall balls for big balls</li>
+                <li>Mark's big booty EMOM</li>
+                <li>AMRAP like everything and die</li>
+                <li></li>
+                <li>Chest-to-Floor burpee</li>
+                <li>Chest-to-Floor burpee</li>
+                <li>Chest-to-Floor burpee</li>
+                <li>Chest-to-Floor burpee</li>
+            </ul>
+        </main>
+    )
+}
